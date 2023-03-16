316ae2b01b1c64faa95440f9ec3fb8433a103409
Youssamo <youssefamroelsaidmohamed@gmail.com>
2023-03-16 16:50:27 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Description: A C program that prints with put function
- *
- * Return: Always 0 (Success)
-*/
-
-int main(void)
-{
-	4-puts.c("\"Programming is like building a multilingual puzzle");
-	return (0);
+#include <stdio.h> 
+ 
+/** 
+ * main - Entry point 
+ * 
+ * Description: A C program that prints with put function 
+ * 
+ * Return: Alawys 0 (Success) 
+*/ 
+ 
+int main(void) 
+{ 
+ puts("\"Programming is like building a multilingual puzzle"); 
+ return (0); 
 }	
