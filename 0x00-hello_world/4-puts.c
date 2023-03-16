9f680342db0e62bc014452d410c350bc1144bd80
Youssamo <youssefamroelsaidmohamed@gmail.com>
2023-03-16 16:55:57 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,16 @@
-#include <stdio.h> 
- 
-/** 
- * main - Entry point 
- * 
- * Description: A C program that prints with put function 
- * 
- * Return: Alawys 0 (Success) 
-*/ 
- 
-int main(void) 
-{ 
- puts("\"Programming is like building a multilingual puzzle"); 
- return (0); 
+#!/bin/bash
+gcc $CFILE -o cisfun
+
+4-puts.c
+
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }	
